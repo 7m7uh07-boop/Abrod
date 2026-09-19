98912232ec7e4d1d242ebf0ff7302280e266a0ae
7m7uh07-boop <7m7uh07@gmail.com>
2026-09-19 16:02:59 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ar" dir="rtl">
-      <body>{children}</body>
+    <html lang="en">
+      <body className="bg-slate-950 text-slate-100 antialiased">{children}</body>
     </html>
   );
 }
